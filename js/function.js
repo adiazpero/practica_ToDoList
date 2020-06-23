0b944461b7eda34a874ed81cab312691bad2e945
adiazpero <anitta_83@hotmail.com>
2020-06-23 10:48:48 +0200

    semi maquetacion

--- js/function.js
+++ js/function.js
@@ -39,7 +39,7 @@ var idTarea = arrTareas.length;
 function pintarTareas(listaTareas) {
     limpiarPantalla();
     for (var i = 0; i < listaTareas.length; i++) {
-        document.getElementById('tareas').innerHTML += `<div class="col-4 ${listaTareas[i].tipo}"><p id="titulo">${listaTareas[i].titulo}</p><p id="descripcion">${listaTareas[i].descripcion}</p><button class="borrar" onclick="eliminarTarea(${listaTareas[i].id})">Eliminar</button></div>`;
+        document.getElementById('tareas').innerHTML += `<div class="col-12 ${listaTareas[i].tipo}"><p id="titulo">${listaTareas[i].titulo}</p><p id="descripcion">${listaTareas[i].descripcion}</p><button class="borrar" onclick="eliminarTarea(${listaTareas[i].id})">Eliminar</button></div>`;
     }
 }
 
